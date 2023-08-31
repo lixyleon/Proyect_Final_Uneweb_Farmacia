fbd37eafa1321de4f85abbb4eba8db79f1709505
lixyleon <123250641+lixyleon@users.noreply.github.com>
2023-08-31 01:10:45 -0400

    Cambiar usuario
    Cambiar usuario de bd en phpadmin para resolver problemas de acceso

--- administrador/config/bd.php
+++ administrador/config/bd.php
@@ -1,11 +1,11 @@
 <?php
 $host="localhost";
     $bd="proyect_farmacia";
-    $usuario="root";
+    $usuario="lleon";
     $contrasenia="";
 
     try {
-        $conexion= new PDO("mysql:host=$host;dbname=$bd",$usuario,$contrasenia);
+        $conexion= new PDO("mysql:host=$host;dbname=$bd,$usuario,$contrasenia");
         //if($conexion){ echo "conectado al sistema";} comprobar conexion
     } catch ( PDOException $ex ) {
         echo $ex->getMessage();
